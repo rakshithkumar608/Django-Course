9004987d1e0a447d6845dc07335d9f4a70709b32
Rakshith Kumar <rakshithkumab01718@gmail.com>
2026-03-25 15:17:22 +0530

    Feat : user registration system

---
+++ myProject14/portfolio/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    city = models.CharField(max_length=100)
+    
+    
+    def __str(self):
+        return self.name
